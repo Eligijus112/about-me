8f15c01dd92d963b0f2a5b0fc423559783f8c66f
Eligijus <eligijus@sixads.net>
2019-07-21 15:53:07 +0300

    added my aws account as an allowed host

--- backend/about_me/settings.py
+++ backend/about_me/settings.py
@@ -12,7 +12,8 @@ class Base(Configuration):
 
     DEBUG = True
 
-    ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+    ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 
+    'ec2-18-219-159-93.us-east-2.compute.amazonaws.com']
 
     # Definng installed applications
 
